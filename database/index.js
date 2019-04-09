a1e83427966d4bd1c5a95deb90e7979f943def4a
Hannah Kim <hannahsohkim@gmail.com>
2019-04-08 19:43:58 -0700

    Parse data before mapping

--- database/index.js
+++ database/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/fetcher');
 
-let repoSchema = mongoose.Schema({
+let repoSchema = new mongoose.Schema({
   id: Number,
   repoName: String,
   ownerLogin: String,
@@ -12,9 +12,9 @@ let repoSchema = mongoose.Schema({
 
 let Repo = mongoose.model('Repo', repoSchema);
 
-let save = (repo) => {
-
-  repo.forEach((repo) => {
+let save = (data) => {
+  data = JSON.parse(data);
+  data.map((repo) => {
     var newRepo = new Repo({
       id: repo.id,
       repoName: repo.name,
@@ -23,20 +23,20 @@ let save = (repo) => {
       description: repo.description,
       watchers: repo.watchers
     })
-    newRepo.save();
-  })
-
-
-
+    newRepo.save((err) => {
+      if(err) {
+        console.log(err)
+      }
+    });
+  });
 }
 
 
-// let topRepos = (callback) => {
-// let top = Repo.find({}).sort({watchers: -1}).limit(25);
-// console.log(top, 'TOP25')
-
-// callback(null, top)
-// }
+let topRepos = (callback) => {
+  let top = Repo.find({}).sort({watchers: -1}).limit(25);
+  console.log(top, 'TOP25')
+  callback(null, top)
+}
 
 module.exports.Repo = Repo;
 module.exports.save = save;
