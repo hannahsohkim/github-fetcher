78475ba3512dea78e56af38243a7c59ac0793897
Hannah Kim <hannahsohkim@gmail.com>
2019-04-08 19:44:32 -0700

    Add styled components

--- client/src/components/RepoList.jsx
+++ client/src/components/RepoList.jsx
@@ -1,19 +1,25 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const RepoList = (props) => {
-  console.log(props.repos, 'REPO')
+  const Header = styled.div`
+    border: 1px solid #99999;
+    border-radius: 3px;
+  `;
+
   return (
   <div>
     <h4> Repo List Component </h4>
     There are {props.repos.length} repos.
     <div>
     {props.repos.map((repo)=> {
-        return (<div>
+        return (
+        <Header>
           <p>Username: {repo.ownerLogin}</p>
           <p>Repo Name: {repo.repoName}</p>
           <p>Description: {repo.description}</p>
           <p>URL: {repo.htmlUrl}</p>
-          </div>
+        </Header>
         )
       })
       }
--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import styled from 'styled-components';
 
-
+const Button = styled.button`
+  border: 1px solid #99999;
+  border-radius: 3px;
+  &:focus {outline: none; box-shadow:0 0 10px #D6D0C4;}
+`;
 
 class Search extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       term: ''
-    }
+    };
+    this.onChange = this.onChange.bind(this);
+    this.search = this.search.bind(this);
   }
 
   onChange (e) {
@@ -25,8 +31,12 @@ class Search extends React.Component {
   render() {
     return (<div>
       <h4>Add more repos!</h4>
-      Enter a github username: <input value={this.state.terms} onChange={this.onChange.bind(this)}/>
-      <button onClick={this.search.bind(this)}> Add Repos </button>
+      Enter a github username:
+      <input
+        value={this.state.terms}
+        onChange={this.onChange}
+      />
+      <button onClick={this.search}> Add Repos </button>
     </div>)
   }
 }
